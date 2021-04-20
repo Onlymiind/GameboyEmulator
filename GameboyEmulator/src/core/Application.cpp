0eda890ae0f2f1cea5dfe446b3f03a9345389794
Onlymiind <new16message@gmail.com>
2021-04-20 15:34:40 +0300

    .

--- GameboyEmulator/src/core/Application.cpp
+++ GameboyEmulator/src/core/Application.cpp
@@ -44,7 +44,7 @@ namespace gbemu {
 	void Application::init()
 	{
 		m_RAM = std::make_unique<RAM>(0x8000, 0xFFFF);
-		m_ROM = std::make_unique<ROM>(FileManager::readFile("../TestRoms/blargg/cpu_instrs/individual/02-interrupts.gb"));
+		m_ROM = std::make_unique<ROM>(FileManager::readFile("../TestRoms/blargg/cpu_instrs/individual/06-ld r,r.gb"));
 
 		m_Bus.connect(MemoryController(0x0000, 0x7FFF, BIND_READ(*m_ROM, &ROM::read), BIND_WRITE(*m_ROM, &ROM::write)));
 		m_Bus.connect(MemoryController(0x8000, 0xFFFF, BIND_READ(*m_RAM, &RAM::read), BIND_WRITE(*m_RAM, &RAM::write)));
--- GameboyEmulator/src/core/gb/AddressBus.cpp
+++ GameboyEmulator/src/core/gb/AddressBus.cpp
@@ -17,10 +17,5 @@ namespace gbemu {
 		auto it = std::find_if(m_Memory.begin(), m_Memory.end(), [address](const MemoryController& controller) { return controller.isInRange(address); });
 
 		if (it != m_Memory.end()) it->write(address, data);
-
-		if (address == 0xFF02 && data == 0x81) {
-			std::cout << static_cast<char>(read(0xFF01)) << "\n";
-			__debugbreak();
-		}
 	}
 }
