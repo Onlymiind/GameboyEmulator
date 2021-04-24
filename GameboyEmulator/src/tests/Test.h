3448d50673230420182409d0ff66ed9bd2b8028a
Onlymiind <new16message@gmail.com>
2021-04-24 17:12:53 +0300

    fix

--- GameboyEmulator/src/tests/Test.h
+++ GameboyEmulator/src/tests/Test.h
@@ -1,43 +1,42 @@
-//#pragma once
-//#include <SFML/Graphics.hpp>
-//
-//
-//#include <memory>
-//#include <string_view>
-//#include <vector>
-//#include <utility>
-//#include <functional>
-//
-//namespace gbemu {
-//
-//
-//	class Test {
-//	public:
-//		Test() {}
-//		~Test() = default;
-//
-//		virtual void update()    = 0;
-//		virtual void updateGUI() = 0;
-//		virtual void render()    = 0;
-//	};
-//
-//
-//	class Menu {
-//	public:
-//		Menu() :
-//			p_Test(nullptr) 
-//		{}
-//		~Menu() = default;
-//
-//		void update();
-//		void updateGUI();
-//		void render(sf::RenderWindow& window);
-//	private:
-//		std::unique_ptr<Test> p_Test;
-//
-//		const std::vector<std::pair<std::string_view, std::function<Test* ()>>> m_TestBase =
-//		{ {
-//
-//		} };
-//	};
-//}
+#pragma once
+
+
+#include <memory>
+#include <string_view>
+#include <vector>
+#include <utility>
+#include <functional>
+
+namespace gbemu {
+
+
+	class Test {
+	public:
+		Test() {}
+		~Test() = default;
+
+		virtual void update()    = 0;
+		virtual void updateGUI() = 0;
+		virtual void render()    = 0;
+	};
+
+
+	class Menu {
+	public:
+		Menu() :
+			p_Test(nullptr) 
+		{}
+		~Menu() = default;
+
+		void update();
+		void updateGUI();
+		void render();
+	private:
+		std::unique_ptr<Test> p_Test;
+
+		const std::vector<std::pair<std::string_view, std::function<Test* ()>>> m_TestBase =
+		{ {
+
+		} };
+	};
+}
