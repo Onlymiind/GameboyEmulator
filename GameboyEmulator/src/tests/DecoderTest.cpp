b3d323c0b5b83e8f57e360ec8c7662c9b6e63e81
Onlymiind <new16message@gmail.com>
2021-07-31 23:31:43 +0300

    no more polymirphism

--- GameboyEmulator/src/tests/DecoderTest.cpp
+++ GameboyEmulator/src/tests/DecoderTest.cpp
@@ -1,6 +1,15 @@
 #include "core/gb/cpu/Decoder.h"
+#include "core/gb/cpu/Decoder.cpp"
 
 #include <cassert>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+const std::vector<std::pair<uint8_t, gb::UnprefixedInstruction>> g_UnprefixedSample = 
+{
+
+};
 
 void TestPrefix()
 {
@@ -11,16 +20,29 @@ void TestPrefix()
 
 void TestDecodingPrefixed()
 {
+    std::cout << "Testing decoding prefixed instructions" << std::endl;
     gb::Decoder decoder;
 
-    gb::Instr sample = 
-    {
-        {},{},{},
-        {gb::ArgumentSource::Register, gb::ArgumentType::Unsigned8}
-    };
+    gb::PrefixedInstruction RLC_B = { gb::InstructionType::RLC, gb::Registers::B};
+    uint8_t RLC_B_code = 0x00;
+    gb::PrefixedInstruction SRA_A = { gb::InstructionType::SRA, gb::Registers::A};
+    uint8_t SRA_A_code = 0x2F;
+    gb::PrefixedInstruction BIT_6_A = {gb::InstructionType::BIT, gb::Registers::A, 6};
+    uint8_t BIT_6_A_code = 0x77;
+    gb::PrefixedInstruction SET_0_B = {gb::InstructionType::SET, gb::Registers::B, 0};
+    uint8_t SET_0_B_code = 0xC0;
+
+    assert(decoder.decodePrefixed(RLC_B_code) == RLC_B);
+    assert(decoder.decodePrefixed(SRA_A_code) == SRA_A);
+    assert(decoder.decodePrefixed(BIT_6_A_code) == BIT_6_A);
+    assert(decoder.decodePrefixed(SET_0_B_code) == SET_0_B);
+
+    std::cout << "OK" << std::endl;
 }
 
 int main()
 {
+    TestPrefix();
+    TestDecodingPrefixed();
     return 0;
 }
