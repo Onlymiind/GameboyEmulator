729f0d3ef83243f391fd1d88047d4632d3a6bad9
Onlymiind <new16message@gmail.com>
2021-09-09 14:13:09 +0300

    cleanup

--- GameboyEmulator/src/Application.h
+++ GameboyEmulator/src/Application.h
@@ -6,13 +6,14 @@
 #include "gb/IO.h"
 #include "gb/InterruptRegister.h"
 #include "gb/cpu/Decoder.h"
+#include "ConsoleInput.h"
 
 #include <memory>
 #include <string_view>
 
 
-namespace gb {
-
+namespace emulator
+{
 	class Application 
 	{
 	public:
@@ -33,18 +34,16 @@ namespace gb {
 
 	private:
 
-		RAM m_RAM;
-		ROM m_ROM;
-		RAM m_Leftover;
-		IORegisters m_GBIO;
-		AddressBus m_Bus;
-		InterruptRegister m_InterruptEnable;
-		InterruptRegister m_InterruptFlags;
-		decoding::Decoder m_Decoder;
-		SharpSM83 m_CPU;
+		gb::RAM m_RAM;
+		gb::ROM m_ROM;
+		gb::RAM m_Leftover;
+		gb::IORegisters m_GBIO;
+		gb::AddressBus m_Bus;
+		gb::InterruptRegister m_InterruptEnable;
+		gb::InterruptRegister m_InterruptFlags;
+		gb::decoding::Decoder m_Decoder;
+		gb::cpu::SharpSM83 m_CPU;
 		
-
-		const uint8_t m_InstructionPerFrame{ 100 };
 		bool m_IsRunning;
 		bool m_EmulatorRunning;
 		bool m_JustStarted;
@@ -52,45 +51,6 @@ namespace gb {
 
 		std::string m_TestPath{ "../TestRoms/blargg/cpu_instrs/individual/" };
 		const std::string m_Extension = ".gb";
-	};
-
-	enum class CommandType
-	{
-		None, Invalid, Help, Quit, SetRomDir, RunRom, List, Config
-	};
-
-	struct Command
-	{
-		CommandType Type;
-		std::string Argument;
-	};
-
-	std::istream& operator >> (std::istream& is, Command& cmd);
-
-	class Parser
-	{
-	public:
-		Parser() = default;
-		~Parser() = default;
-
-		Command parse(std::string_view text) const;
-
-	private:
-
-		struct CommandInfo
-		{
-			std::string_view Name;
-			CommandType Type;
-			bool HasArguments;
-		};
-
-		std::string getArguments(std::string_view text, const CommandInfo& info) const;
-
-		const std::array<CommandInfo, 6> m_Commands =
-		{ {
-			{"-help", CommandType::Help, false}, {"-quit", CommandType::Quit, false},
-			{"-romdir", CommandType::SetRomDir, true}, {"-run", CommandType::RunRom, true},
-			{"-ls", CommandType::List, false}, {"-config", CommandType::Config, false}
-		} };
+		Parser m_CommandParser;
 	};
 }
