8d1eed6e58c18208ee11874f8784e404ce7505e9
Onlymiind <new16message@gmail.com>
2021-09-10 14:15:16 +0300

    output

--- GameboyEmulator/src/ConsoleOutput.cpp
+++ GameboyEmulator/src/ConsoleOutput.cpp
@@ -1,1 +1,57 @@
 #include "ConsoleOutput.h"
+#include <iomanip>
+
+#define FMT_HEX(x) "0x" << std::setfill('0') << std::setw(sizeof(x) * 2) << std::hex << +x
+
+namespace emulator
+{
+    void Printer::printTitle() const
+    {
+        output_ << R"(
+****************************
+*  Welcome to GB emulator  *
+****************************
+            )" << '\n';
+    }
+
+    void Printer::printHelp() const
+    {
+        output_ << R"(
+            -help - show this text
+            -romdir <path> -set ROM directory
+            -run <name> -run a ROM
+            -ls - list all ROMs in current directory
+            -config - show current ROM directory
+            -quit - quit the emulator)" << '\n';
+    }
+
+    void Printer::print(std::string_view text) const
+    {
+        output_ << text << '\n';
+    }
+
+    void Printer::reportError(InputError err) const
+    {
+        switch(err)
+        {
+            case InputError::InvalidCommand:
+                output_ << "Invalid command. Use -help for command list";
+                break;
+            case InputError::InvalidDirectory:
+                output_ << "Selected directory doesn't exist";
+                break;
+            case InputError::InvalidRomName:
+                output_ << "ROM doesn't exist in the specified directory";
+                break;
+        }
+
+        output_ << '\n';
+    }
+
+    void Printer::reportError(std::string_view description, uint16_t program_counter) const
+    {
+        output_ << "Emulation terminated due to runtime error.\n"
+            << "Error description: " << description << '\n'
+            << "CPU program counter: " << FMT_HEX(program_counter) << '\n';
+    }
+}
--- GameboyEmulator/src/tests/InterruptRegisterTest.cpp
+++ GameboyEmulator/src/tests/InterruptRegisterTest.cpp
@@ -11,24 +11,24 @@ void TestInterruptRegister()
 {
     gb::InterruptRegister reg;
 
-    assert(reg.getFlags() == gb::g_UnusedInterruptBits);
+    assert(reg.getFlags() == gb::g_unused_interrupt_bits);
 
     reg.setFlag(gb::InterruptFlags::LCD_STAT);
     assert(reg.read(0x0000) == reg.getFlags());
-    assert(reg.getFlags() == (gb::g_UnusedInterruptBits | static_cast<uint8_t>(gb::InterruptFlags::LCD_STAT)));
+    assert(reg.getFlags() == (gb::g_unused_interrupt_bits | static_cast<uint8_t>(gb::InterruptFlags::LCD_STAT)));
 
     reg.clearFlag(gb::InterruptFlags::LCD_STAT);
-    assert(reg.getFlags() == gb::g_UnusedInterruptBits);
+    assert(reg.getFlags() == gb::g_unused_interrupt_bits);
 
     reg.clearFlag(gb::InterruptFlags::Joypad);
-    assert(reg.getFlags() == gb::g_UnusedInterruptBits);
+    assert(reg.getFlags() == gb::g_unused_interrupt_bits);
 
-    reg.write(0x0000, gb::g_UnusedInterruptBits | static_cast<uint8_t>(gb::InterruptFlags::VBlank));
+    reg.write(0x0000, gb::g_unused_interrupt_bits | static_cast<uint8_t>(gb::InterruptFlags::VBlank));
     assert(reg.read(0x0000) == reg.getFlags());
-    assert(reg.getFlags() == (gb::g_UnusedInterruptBits | static_cast<uint8_t>(gb::InterruptFlags::VBlank)));
+    assert(reg.getFlags() == (gb::g_unused_interrupt_bits | static_cast<uint8_t>(gb::InterruptFlags::VBlank)));
 
     reg.write(0x0000, 0x00);
-    assert(reg.getFlags() == gb::g_UnusedInterruptBits);
+    assert(reg.getFlags() == gb::g_unused_interrupt_bits);
 }
 
 int main()
