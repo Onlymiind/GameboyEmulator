430d5d1a78ef53b8db483ea3f1258be9399013c0
onlymind <sasha0darmostuk@gmail.com>
2021-10-06 13:01:17 +0300

    coroutines

--- GameboyEmulator/src/utils/Utils.h
+++ GameboyEmulator/src/utils/Utils.h
@@ -7,6 +7,8 @@
 #include <filesystem>
 #include <string_view>
 #include <sstream>
+#include <queue>
+#include <functional>
 
 std::vector<uint8_t> readFile(const std::filesystem::path& fileName);
 
@@ -22,4 +24,48 @@ std::string printToString(std::string_view separator, Args... args)
 	std::stringstream stream;
 	((stream << separator << args), ...);
 	return stream.str();
+}
+
+template<typename Context, typename Result>
+class Coroutine
+{
+public:
+	template<typename Container>
+	Coroutine(const Context& ctxt, const Result& res, const Container& instructions)
+		: context_(ctxt), result_(res), instructions_(instructions)
+	{}
+
+	bool IsFinished() const;
+	Result GetResult() const;
+
+	void operator()();
+private:
+	Context context_;
+	Result result_;
+	std::queue<std::function<void(Context&, Result&)>> instructions_;
+};
+
+template<typename Context, typename Result>
+bool Coroutine<Context, Result>::IsFinished() const
+{
+	return instructions_.empty();
+}
+
+template<typename Context, typename Result>
+Result Coroutine<Context, Result>::GetResult() const
+{
+	return result_;
+}
+
+template<typename Context, typename Result>
+void Coroutine<Context, Result>::operator()()
+{
+	if(instructions_.empty())
+	{
+		//TODO: maybe throw an exception
+		return;
+	}
+
+	instructions_.front()(context_, result_);
+	instructions_.pop();
 }
