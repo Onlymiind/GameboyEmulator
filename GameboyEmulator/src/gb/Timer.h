26b574fbef659d5fb56a6c2eafff591f89458fa1
Onlymiind <new16message@gmail.com>
2021-11-14 22:45:04 +0300

    pipelines, fixed timer

--- GameboyEmulator/src/gb/Timer.h
+++ GameboyEmulator/src/gb/Timer.h
@@ -21,8 +21,9 @@ namespace gb
         void write(uint16_t address, uint8_t data) override;
 
     private:
-        cpu::Word counter_ = 0xABCC;
-        uint8_t& DIV_ = counter_.getHight();
+        uint8_t DIV_ = 0xAB;
+        uint8_t align_ = 0xCC;
+        cpu::Word counter_ = cpu::Word(DIV_, align_);
 
         uint8_t TIMA_ = 0;
         uint8_t TMA_ = 0;
--- GameboyEmulator/src/gb/cpu/Instructions.h
+++ GameboyEmulator/src/gb/cpu/Instructions.h
@@ -1,10 +1,60 @@
 #pragma once
 #include "gb/cpu/CPUUtils.h"
+#include "gb/cpu/Operation.h"
+
+#include <memory>
 
 namespace gb
 {
     namespace cpu
     {
+        //TODO: implement instructions as different pipelines
+        //This shouldn't be difficult and seems like good idea
+        class Pipeline
+        {
+        public:
+            virtual ~Pipeline() = default;
+
+            virtual bool nextStage() = 0;
+
+            static std::unique_ptr<Pipeline> newPipeline(const decoding::Decoder& decoder, RegisterFile& registers, decoding::opcode opcode);
+        protected:
+            RegisterFile& registers_;
+            const decoding::Decoder& decoder_;
+            decoding::opcode opcode_;
+            uint8_t stage_count_ = 0;
+        };
+
+        class ALUPipeline : public Pipeline
+        {
+        public:
+        private:
+        };
+
+        class ADDpipeline : public Pipeline
+        {
+
+        };
+
+        class LDPipeline : public Pipeline
+        {
+
+        };
+
+        class JRPipeline : public Pipeline
+        {
+
+        };
+
+        class JPPipeline : public Pipeline
+        {
+
+        };
+
+        class CALLpipeline : public Pipeline
+        {
+
+        };
 
         void addByte(InstructionContext& context);
         void addSP(InstructionContext& context);
